b4579d5d0f48f39c7c172e39eaa9e06a2ba0c455
Yue Yihua <159306110+easygl1der@users.noreply.github.com>
2026-09-12 00:16:13 +0800

    Add Method B Header2Section assembled JSX

---
+++ perfect-web-clone/src/components/sections/header-2/Header2Section.jsx
@@ -0,0 +1,227 @@
+import "./Header2Section.css";
+// Header2Section — authored from pwc section evidence.
+export default function Header2Section() {
+  return (
+      <>
+        <div className="uk-container uk-container-large archive-header-container the-sub-nav">
+          <div className="uk-grid archive-header-grid">
+            <div className="fbsubnav uk-section uk-padding-remove-vertical uk-width-1-1" style={{ paddingLeft: '0px' }}>
+              <div className="uk-container-expand">
+                <div className="uk-grid uk-grid-collapse">
+                  <div className="uk-width-generic uk-padding-remove uk-flex uk-flex-middle sub-nav-container main-nav-column" id="sub-nav-container">
+                    <button aria-expanded="false" aria-label="" className="menu-toggle swaporder1" id="submenu-toggle" title="">
+                      <div className="menu-toggle-inner">
+                        <div className="toggle-bar toggle-bar-1" />
+                        <div className="toggle-bar toggle-bar-2" />
+                        <div className="toggle-bar toggle-bar-3" />
+                        <div className="toggle-bar toggle-bar-4" />
+                      </div>
+                    </button>
+                    <div className="subnav-content-wrapper uk-flex uk-flex-middle uk-width-1-1">
+                      <nav aria-label="Main" className="main-navigation uk-flex-1" data-pwc-critical="navigation-2" id="site-subnavigation">
+                        <ul aria-expanded="false" className="subnav-menu uk-align-left subnav-menu" id="subnav-menu">
+                          <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-44468 menu-item-depth-0" id="menu-item-44468">
+                            <a aria-expanded="false" aria-label="Technologies" className="menu-link mega-toggle a_main_menu_link" href="#">
+                              Technologies
+                            </a>
+                            <div className="mega mega-depth-0 top-mega">
+                              <div className="mega-inner top-mega-inner uk-clearfix uk-hidden@m">
+                                <div className="mega-desc top-mega-desc">
+                                  <div className="mega-desc-inner">
+                                    <p className="menu-desc-preheading-placeholder" />
+                                  </div>
+                                </div>
+                                <ul className="sub-menu menu-depth-0">
+                                  <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-44469 menu-item-depth-1" id="menu-item-44469">
+                                    <a aria-expanded="false" aria-label="Menu" className="menu-link sub-mega-toggle a_main_menu_link" data-depth0="Technologies" href="#">
+                                      Menu
+                                    </a>
+                                    <div className="mega mega-depth-1 top-mega">
+                                      <div className="mega-inner sub-mega-inner uk-clearfix">
+                                        <div className="mega-desc sub-mega-desc">
+                                          <div className="mega-desc-inner">
+                                            <p className="menu-desc-preheading-placeholder" />
+                                          </div>
+                                        </div>
+                                        <ul className="sub-menu menu-depth-1">
+                                          <li className="menu-item menu-item-type-custom menu-item-object-custom menu-item-44470 menu-item-depth-2" id="menu-item-44470">
+                                            <a className="menu-link a_main_menu_link" data-depth0="Technologies" data-depth1="Menu" href="#">
+                                              Menu
+                                            </a>
+                                          </li>
+                                        </ul>
+                                        <button aria-label="Go up one level" aria-pressed="false" className="mega-back" tabIndex="0" title="Go up one level">
+                                          <svg height="14px" version="1.1" viewBox="0 0 69 14" width="69px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                            <g fill="none" fillRule="evenodd" id="navui_back_wrap" stroke="none" strokeWidth="1">
+                                              <g className="navui-back" fill="#3A3A3A" id="navui_back" transform="translate(0.000000, -1.000000)">
+                                                <g fillRule="nonzero" id="navui_back_arrow" transform="translate(0.000000, 1.000000)">
+                                                  <polygon id="Path-Copy-2" points="6.96303297 13.9260659 1.77635684e-15 6.96303297 6.96303297 0 8.19393493 1.20674571 3.30843889 6.09265385 18.9286108 6.09265385 18.9286108 7.83341209 3.33211321 7.83341209 8.19393493 12.695164" />
+                                                </g>
+                                              </g>
+                                            </g>
+                                          </svg>
+                                          <span className="mega-back-label">
+                                            Back
+                                          </span>
+                                        </button>
+                                        <button aria-label="Close submenu" aria-pressed="false" className="mega-close" tabIndex="0" title="Close submenu">
+                                          <svg height="20px" version="1.1" viewBox="0 0 20 20" width="20px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                            <g fill="none" fillRule="evenodd" id="navui_close" stroke="none" strokeWidth="1">
+                                              <g fill="#3A3A3A" fillRule="nonzero" id="navui_close_x">
+                                                <path d="M2.56410256,9.09494702e-13 L10,8.529 L17.4358974,0 L20,0 L11.282,10 L20,20 L17.4358974,20 L10,11.471 L2.56410256,20 L3.97903932e-13,20 L8.718,10 L0,9.09494702e-13 L2.56410256,9.09494702e-13 Z" id="Combined-Shape-Copy-2" />
+                                              </g>
+                                            </g>
+                                          </svg>
+                                        </button>
+                                      </div>
+                                    </div>
+                                  </li>
+                                </ul>
+                                <button aria-label="Close submenu" aria-pressed="false" className="mega-close" tabIndex="0" title="Close submenu">
+                                  <svg height="20px" version="1.1" viewBox="0 0 20 20" width="20px" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                                    <g fill="none" fillRule="evenodd" id="navui_close" stroke="none" strokeWidth="1">
+                                      <g fill="#3A3A3A" fillRule="nonzero" id="navui_close_x">
+                                        <path d="M2.56410256,9.09494702e-13 L10,8.529 L17.4358974,0 L20,0 L11.282,10 L20,20 L17.4358974,20 L10,11.471 L2.56410256,20 L3.97903932e-13,20 L8.718,10 L0,9.09494702e-13 L2.56410256,9.09494702e-13 Z" id="Combined-Shape-Copy-2" />
+                                      </g>
+                                    </g>
+                                  </svg>
+                                </button>
+                              </div>
+                              <div className="twocolumn-mm our_technologies_sub uk-visible@m animated animated2" style={{ display: 'none' }}>
+                                <div className="uk-width-1-1@s footer-column-two uk-visible@m subnavi">
+                                  <div className="footer-column-widget menu-column-widget-one widget_nav_menu" id="nav_menu-45">
+                                    <div className="menu-sub-nav-our-technologies-container">
+                                      <ul className="menu" id="menu-sub-nav-our-technologies">
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-post_tag menu-item-44477" id="menu-item-44477">
+                                          <a className="a_main_menu_link" href="/news/tag/ai/">
+                                            AI
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44478" id="menu-item-44478">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/facebook-app/">
+                                            Facebook
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44479" id="menu-item-44479">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/instagram/">
+                                            Instagram
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44480" id="menu-item-44480">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/messenger/">
+                                            Messenger
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44481" id="menu-item-44481">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/oculus/">
+                                            Meta Quest
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44482" id="menu-item-44482">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/threads/">
+                                            Threads
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44483" id="menu-item-44483">
+                                          <a className="a_main_menu_link" href="/news/category/technologies/whatsapp/">
+                                            WhatsApp
+                                          </a>
+                                        </li>
+                                        <li className="menu-item menu-item-type-taxonomy menu-item-object-post_tag menu-item-44484" id="menu-item-44484">
+                                          <a className="a_main_menu_link" href="/news/tag/ray-ban-meta-glasses/">
+                                            AI Glasses
+                                          </a>
+                                        </li>
+                                      </ul>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-44473 menu-item-depth-0 current-item-parent" id="menu-item-44473">
+                            <a className="menu-link a_main_menu_link" href="/news/category/product-news/">
+                              Product News
+                            </a>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-category current-post-ancestor current-menu-parent current-post-parent menu-item-44475 menu-item-depth-0 current-item-parent" id="menu-item-44475">
+                            <a className="menu-link a_main_menu_link" href="/news/category/technology-and-innovation/">
+                              Innovation
+                            </a>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44474 menu-item-depth-0" id="menu-item-44474">
+                            <a className="menu-link a_main_menu_link" href="/news/category/public-policy/">
+                              Public Policy
+                            </a>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-post_tag menu-item-44476 menu-item-depth-0" id="menu-item-44476">
+                            <a className="menu-link a_main_menu_link" href="/news/tag/well-being/">
+                              Youth Well-Being
+                            </a>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-44471 menu-item-depth-0" id="menu-item-44471">
+                            <a className="menu-link a_main_menu_link" href="/news/category/data-and-privacy/">
+                              Privacy
+                            </a>
+                          </li>
+                          <li className="menu-item menu-item-type-taxonomy menu-item-object-post_tag menu-item-44504 menu-item-depth-0" id="menu-item-44504">
+                            <a className="menu-link a_main_menu_link" href="/news/tag/security/">
+                              Security
+                            </a>
+                          </li>
+                        </ul>
+                      </nav>
+                      <div className="site-search-container header-search-container no-header-query" id="header_search_form_sub">
+                        <div className="header-search-inner">
+                          <div className="header-search-body">
+                            <form action="/" className="uk-search uk-search-navbar" id="search_form_sub" method="get">
+                              <span className="search-glass-icon">
+                                <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                  <path d="M2.545 2.546c3.349-3.35 8.746-3.4 12.031-.115 3.048 3.048 3.223 7.914.558 11.275.438.434 3.148 3.144 4.22 4.216l.41.41c.356.355.29.998-.072 1.36-.363.363-1.005.428-1.36.072-2.74-2.739-4.283-4.28-4.633-4.625-3.36 2.66-8.222 2.483-11.268-.563-3.286-3.285-3.234-8.682.114-12.03zm10.717 1.428a6.575 6.575 0 00-9.288 0 6.575 6.575 0 000 9.288 6.575 6.575 0 009.288 0 6.575 6.575 0 000-9.288z" fill="currentColor" fillRule="evenodd" />
+                                </svg>
+                              </span>
+                              <label className="search-label" htmlFor="header_search_input_sub">
+                                Search News
+                              </label>
+                              <div className="header-search-input-container" id="js-header_search_input_sub_container">
+                                <div className="autocomplete__wrapper">
+                                  <div style={{ border: '0px', clip: 'rect(0px, 0px, 0px, 0px)', height: '1px', marginBottom: '-1px', marginRight: '-1px', overflow: 'hidden', padding: '0px', position: 'absolute', whiteSpace: 'nowrap', width: '1px' }}>
+                                    <div aria-atomic="true" aria-live="polite" id="header_search_input_sub__status--A" role="status" />
+                                    <div aria-atomic="true" aria-live="polite" id="header_search_input_sub__status--B" role="status" />
+                                  </div>
+                                  <input aria-autocomplete="list" aria-describedby="header_search_input_sub__assistiveHint" aria-expanded="false" aria-owns="header_search_input_sub__listbox" autoComplete="off" className="autocomplete__input autocomplete__input--default" id="header_search_input_sub" name="s" placeholder="" role="combobox" type="text" />
+                                  <ul className="autocomplete__menu autocomplete__menu--inline autocomplete__menu--hidden" id="header_search_input_sub__listbox" role="listbox" />
+                                  <span id="header_search_input_sub__assistiveHint" style={{ display: 'none' }}>
+                                    When autocomplete results are available use up and down arrows to review and enter to select.  Touch device users, explore by touch or with swipe gestures.
+                                  </span>
+                                </div>
+                              </div>
+                              <button className="header-search-reset sub" id="header_search_reset_sub" type="reset">
+                                Clear
+                              </button>
+                            </form>
+                            <div className="uk-container uk-container-expand search-results-container">
+                              <div className="uk-grid">
+                                <div className="search-results-list">
+                                  <div className="search-results-all">
+                                    <a href="javascript:{}" id="search_query_link_sub" onclick="document.getElementById('search_form_sub').submit(); return false;">
+                                      see all results for
+                                      <span id="search_dynamic_update_sub" />
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+  );
+}
